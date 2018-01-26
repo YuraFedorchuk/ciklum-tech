7eea7dd98d9e49d69cf25fb20f995afb229fbe3e
Yurii <imafost758@gmail.com>
2018-01-26 11:28:23 +0200

    process expired handling updated

--- core/core.js
+++ core/core.js
@@ -2,14 +2,12 @@ const api           = require('./../api');
 const logger        = require('./logger');
 const processFatal  = require('./fatalHandler');
 
-try {
-    api.processExpired()
-        .then()
-        .catch(err => {
-            throw err;
-        }); 
-} catch(err) {
-    logger.error(__filename, 'expired messages processing error', err);
-}
+
+api.processExpired()
+    .then()
+    .catch(err => {
+        logger.error(__filename, 'expired messages processing error', err);
+    }); 
+
 
 process.on('uncaughtException', processFatal);
